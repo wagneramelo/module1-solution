9d6e415a8346f71fef7af2d8a79347fab8c77abb
Wagner Augusto Melo De Paulo <wagner.paulo@ihm.local>
2017-04-17 11:33:00 -0300

    Second Try

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', ['$scope', function($scope){
 	$scope.dishes = '';
 	var qtyDishes = [];
+	$scope.msg = '';
 	
 
-	$scope.splitIt = function(item){
+	$scope.checkLunch = function(item){
 		
 		var re = /\s*,\s*/;
 
 			if($scope.dishes == undefined){
-				alert('Please insert a data!')
+				$scope.msg = 'Please insert a data!';
 			}
 			else{
 				qtyDishes = item.split(re);
 
 				if(qtyDishes.length<=3){
-					alert('Enjoy!');
+					$scope.msg = 'Enjoy!';
 				}
 				else{
-					alert('Too Much!!');
+					$scope.msg = 'Too Much!!';
 				}
 			}
 			$scope.dishes = undefined; //reset the form
